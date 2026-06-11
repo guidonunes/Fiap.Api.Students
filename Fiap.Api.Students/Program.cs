7ebc95cf4809c7e78200f8a33e8a60982e5aec5f
Guilherme Nunes <guilherme.augd@gmail.com>
2026-06-11 16:20:48 -0300

    chore: add entity framework dependencies

--- Fiap.Api.Students/Program.cs
+++ Fiap.Api.Students/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+#region DATA BASE INITIALIZATION
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(true)
+);
+
+#endregion
+
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
