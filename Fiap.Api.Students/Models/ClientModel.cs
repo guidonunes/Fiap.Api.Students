cc063406d448f84fca2ae5cb3750cb3ac02c3c76
Guilherme Nunes <guilherme.augd@gmail.com>
2026-06-11 15:17:28 -0300

    chore: add models directory & client and representative models

---
+++ Fiap.Api.Students/Models/ClientModel.cs
@@ -0,0 +1,13 @@
+namespace Fiap.Api.Students.Models;
+
+public class ClientModel
+{
+    public int ClientId { get; set; }
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public string? Observation {get; set;}
+    public int RepresentativeId { get; set; }
+    public RepresentativeModel? Representative { get; set;}
+}
