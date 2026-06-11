d0d41c016733d713f6614cf7e08d46429a0b0b01
Guilherme Nunes <guilherme.augd@gmail.com>
2026-06-11 15:42:41 -0300

    chore: add error view model

---
+++ Fiap.Api.Students/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fiap.Api.Students.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
