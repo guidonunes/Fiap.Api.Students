49cd70cd2b8a578ae46bc17fc1aebb47d17eefdb
Guilherme Nunes <guilherme.augd@gmail.com>
2026-06-11 22:40:06 -0300

    add client view model

---
+++ Fiap.Api.Students/ViewModel/ClientViewModel.cs
@@ -0,0 +1,13 @@
+namespace Fiap.Api.Students.ViewModel;
+
+public class ClientViewModel
+{
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime BirthDate{ get; set; }
+    public string Observation { get; set; }
+    public int RepresentativeId { get; set; }
+    public RepresentativeViewModel Representative { get; set; }
+}
